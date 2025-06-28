a3c7433f02c2067a621c5a79b6adc1ad334f3c9a
Frankish0014 <f.ishimwe@alustudent.com>
2025-06-28 12:42:45 +0200

    collect signs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 """ start teh code with function to gather infomation """
 
 if __name__ == "__main__":
-    employee_id = sys.argv(1)
+    employee_id = sys.argv[1]
     rest_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id) ## pasing and formating employee_id as .formate(employee_id)
 
     ## Extract and hold todos from 
